27a03113cdaa28ae7e3f3300a8f6cf2e5fa5de50
Danurai <Danurai@users.noreply.github.com>
2018-02-20 17:45:06 +0000

    Add files via upload

--- src/hotac/core.cljs
+++ src/hotac/core.cljs
@@ -1,29 +1,24 @@
 (ns hotac.core
-   (:require [reagent.core :as r]))
+   (:require [reagent.core :as r]
+            [hotac.datastore :as ds]))
    
 (enable-console-print!)
 
 (def app-data (r/atom nil))
+(def app-state (r/atom nil))
 
-(def xwing {:type "X-Wing" 
-           :attack 3 :evade 2 :hull 3 :shields 2 :actions ["Focus" "Target Lock"]
-           :maneuvers [[0,0,0,0,0,0],[0,2,2,2,0,0],[1,1,2,1,1,0],[1,1,1,1,1,0],[0,0,1,0,0,3]]
-           :slots ["Modification" "Astromech" "Torpedo"]
-           :symbol "x" :xp 5 :info "The X-wing is a well-rounded campaign workhorse, and is great for cleaning up enemy fighters. For Pilots looking to switch to an A-wing or B-wing later, it tends to be the most efficient starting choice."})
-(def ywing {:type "Y-Wing" 
-           :attack 2 :evade 1 :hull 3 :shields 2 :actions ["Focus" "Target Lock"] 
-           :maneuvers [[0,0,0,0,0,0],[0,1,2,1,0,0],[1,1,2,1,1,0],[3,1,1,1,3,0],[0,0,3,0,0,3]]
-           :slots ["Modification" "Astromech" "Torpedo" "Torpedo" "Turret"]
-           :special ["Title" "BTL-A4"]
-           :symbol "y" :xp 8 :info "Capable of carrying a turret and multiple types of ordnance, the Y-wing is flexible enough to take on any campaign mission. Bomb Loadout is factored into the scoresheet and not limited here, and BTL-A4 can give any turreted Y-wing extra punch."})
 
 (defn- new-pilot [ship]
-   (prn (->> @app-data :pilots count))
-   (assoc ship :id (if (= 0 (->> @app-data :pilots count))
-                     0
-                     (->> @app-data :pilots (map :id) (apply max) inc))
-              :callsign nil
-              :ps 2))
+  {:xp (:startxp ship)
+   :id (if (= 0 (->> @app-data :pilots count))
+          0
+          (->> @app-data :pilots (map :id) (apply max) inc))
+   :ps 2
+   :ship ship
+   :upgradeslots (map-indexed (fn [idx x] {:id idx :type x}) (:slots ship))
+   })
+           
+;;[{:symbol "A" :options ["R2 Astromech" "R5-X3"]}] 
            
 (defn- statline [stats]
    (let [statsymbols {:ps "x" :attack "%" :evade "^" :hull "&" :shield "*"}]
@@ -41,19 +36,19 @@
                ^{:key move}[:td {:class (str "move move-" (val move))} (key move)])])]])
 
 (defn- new-squad []
-   (reset! app-data {:squadname nil :pilots []})
-   (prn app-data))
+   (reset! app-data {:squadname nil :pilots []}))
    
 (defn- add-pilot [ship]
    (swap! app-data update :pilots conj (new-pilot ship)))
    
 (defn- ship-button [ship]
+   ^{:key ship}
    [:button.btn.btn-outline-secondary
       {:data-dismiss "modal"
        :on-click #(add-pilot ship)}
       [:span.h3
          [:span.h1.xwing-ships.mr-1 (:symbol ship)]
-         [:span.xwing-font-b (:type ship)]
+         [:span.xwing-font-b (:name ship)]
          [:span.badge.badge-secondary.ml-2 (statline (select-keys ship [:attack :evade :hull :shields]))]]
       [:p.font-italic.xwing-btn-wrap (:info ship)]])
 
@@ -66,8 +61,8 @@
                [:button.close {:data-dismiss "modal"}
                   [:span "x"]]]
             [:div.modal-body
-               (ship-button xwing)
-               (ship-button ywing)]]]])
+               (map #(ship-button %) ds/ships)]]]])
+            
 (defn- do-modal-action []
    (let [action (-> @app-data :confirm :action)]
       (case (:type action)
@@ -92,7 +87,7 @@
 (defn- navbar []
    [:nav.navbar.navbar-dark.bg-dark.justify-content-between.mb-2
       [:span.navbar-brand.h1 "HotAC - Squad Builder"]
-      [:button.btn.btn-primary {:on-click new-squad} "New Squadron"]])
+      [:button.btn.btn-primary {:on-click new-squad} "New Squad"]])
       
 (defn render-squad-header []
    [:div.input-group.mb-3
@@ -106,39 +101,95 @@
          [:button.btn.btn-outline-primary 
             {:data-toggle "modal" :data-target "#select-ship"} "Add Pilot"]]])
 
-(defn render-pilot [idx pilot]
-   ^{:key (:id pilot)}[:div.border-left.border-primary.mt-2.mb-2
+(defn- setmodal-delete-pilot [pilot]
+   (swap! app-data assoc :confirm {:message (str "Are you sure you want to permanently delete the " (:type pilot) " pilot " (if (some? (:callsign pilot)) (:callsign pilot) "with no name") "?")
+                                :action {:type :delete-pilot :idx (:id pilot)}}))
+(defn- update-pilot [id key value]
+   (swap! app-data assoc :pilots (map #(if (= id (:id %)) (assoc % key value) % ) (:pilots @app-data) )))
+
+(defn- assign-upgrade [pilotid slot upgradeid upgrades]
+   (let [pidx (->> @app-data :pilots (take-while #(not= 0 (:id %))) count)]
+      (swap! app-data assoc-in [:pilots pidx :upgrades] (map #((if (= upgradeid (:id %)) (assoc % :slot slot))
+                                                         (if (= slot (:slot %)) (dissoc % :slot))) upgrades))))
+   
+(defn- input-group-select [pid upgrade options]
+   ^{:key (:id upgrade)}[:div.input-group.mb-1
+      [:div.input-group-prepend
+         [:span.input-group-text.xwing-symbols (ds/slot-symbol (:type upgrade))]]
+      [:select.form-control {:value (->> options (filter #(= (:slot %) (:id upgrade))) first :name) 
+                          :defaultValue (str "No " (:type upgrade) " selected")
+                          :on-change #(assign-upgrade pid (:id upgrade) (.-id (aget (-> % .-target .-options) (-> % .-target .-selectedIndex))) options)}
+         (for [opt (concat [{:name (str "No " (:type upgrade) " selected") :default true}] options)]
+            ^{:key opt}[:option {:hidden (:default opt) :id (:id opt)}
+                        (:name opt) ])]
+      [:div.input-group-append
+         [:span.input-group-text.btn.btn-secondary "+"]]])
+  
+(defn render-pilot [pilot]
+   ^{:key (:id pilot)}
+   [:div.container-fluid.border-left.border-danger.mt-2.mb-2
       [:div.row-fluid
-         [:div (str pilot)]
+            ;;[:div (str pilot)]
          [:div.col-sm-12
             [:div.input-group.mb-1
                [:div.input-group-prepend
-                  [:span.input-group-text "Callsign"]]
-               [:input.form-control {:placeholder "Your callsign here" 
+                  [:span.input-group-text "Callsigns"]]
+               [:input.form-control {:placeholder "Your Callsign here"
                                   :value (:callsign pilot)
-                                  :on-change #(swap! app-data update-in [:pilots idx] assoc :callsign (-> % .-target .-value))}]
+                                  :on-change #(update-pilot (:id pilot) :callsign (-> % .-target .-value))}]
+               [:input.form-control.col-3 {:placeholder "Pilot name"
+                                  :value (:player pilot)
+                                  :on-change #(update-pilot (:id pilot) :player (-> % .-target .-value))}]
                [:button.btn.btn-danger.ml-1
                   {:data-toggle "modal" 
                    :data-target "#confirm-modal" 
-                   :on-click #(swap! app-data assoc :confirm {:message (str "Are you sure you want to permanently delete the " (:type pilot) " pilot " (if (some? (:callsign pilot)) (:callsign pilot) "with no name") "?")
-                                                         :action {:type :delete-pilot :idx (:id pilot)}})}
-                  "x"]]]]])
+                   :on-click #(setmodal-delete-pilot pilot)} "x"]]
+            [:div.form-inline
+               [:div.input-group.mb-1.mr-1
+                  [:div.input-group-prepend 
+                     [:span.input-group-text "Ship"]]
+                  [:span.form-control (-> pilot :ship :name)]
+                  [:div.input-group-append
+                     [:span.input-group-text.btn.btn-secondary "+"]]]
+               [:div.input-group.mb-1.mr-1
+                  [:div.input-group-prepend
+                     [:span.input-group-text "XP"]]
+                  [:input.form-control {:type "number" 
+                          :value (:xp pilot)
+                          :on-change #(update-pilot (:id pilot) :xp (-> % .-target .-value))}]]
+               [:div.input-group.mb-1
+                  [:div.input-group-prepend
+                     [:span.input-group-text "PS"]]
+                  [:input.form-control {:type "number" 
+                          :value (:ps pilot)
+                          :on-change #(update-pilot (:id pilot) :ps (-> % .-target .-value))}]
+                  [:div.input-group-append
+                     [:span.input-group-text.btn.btn-secondary "+"]]]]]]
+      [:div.row-fluid
+         [:div.col-sm-6
+            (doall 
+               (map (fn [upgrade] (input-group-select (:id pilot) upgrade (->> pilot :upgrades (filter #(= (:type upgrade) (:type %))))))
+                  (:upgradeslots pilot))) ]]])
       
 (defn draw-page []
-   [:div.container
+   [:div.container.bg-light
       (ship-modal)
       (confirm-modal)
       (navbar)
       (if (some? @app-data) (render-squad-header))
-      (doall (map-indexed (fn [idx pilot] (render-pilot idx pilot)) (:pilots @app-data)))])
+      (doall (map #(render-pilot %) (:pilots @app-data)))])
 
 ;; For testing app state
 (def info 
    {:squadname nil
-    :pilots []})
+    :pilots [(new-pilot (last ds/ships))]})
 
 (defn render-page []
    (reset! app-data info)
+   (swap! app-data assoc-in [:pilots 0 :upgrades] [{:id 0 :type "Astromech" :name "R2 Astromech" :xws "r2astromech" :slot 0}
+                                              {:id 1 :type "Astromech" :name "R3-X5"}
+                                              {:id 2 :type "Torpedo" :name "Proton Torpedos" :slot 1}
+                                              {:id 7 :type "Torpedo" :name "Ion Torpedos"}])
    (r/render [draw-page] (.getElementById js/document "app")))
 
 
